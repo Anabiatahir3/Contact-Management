b2fb67a7efb23ea20802c76989e3e998c2c36bca
Anabiatahir3 <anabiatahir365@gmail.com>
2021-11-02 00:30:39 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app=express();
+app.get('/',(req,res)=>
+{
+    res.send("hello world")
+})
+const port=5000;
+app.listen(port,()=>console.log(`server started on ${port}`));
